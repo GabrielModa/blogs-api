684d308bcff8fdd9010e34b0f5732d46f5d4197f
Gabriel Moda <gabriel-moda@hotmail.com.br>
2022-03-17 23:00:47 -0300

    refatoring, exports

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 require('dotenv').config();
 const express = require('express');
-
 require('express-async-errors');
-const { errorHandler } = require('./middleware');
 
-const userRouter = require('./routes/user.routes');
-const loginRouter = require('./routes/login.routes');
-
-const schemaHandler = require('./middleware/schemaHandler');
-const loginSchema = require('./schemas/loginSchema');
-const userSchema = require('./schemas/userSchema');
+const { errorHandler, schemaHandler } = require('./middleware');
+const { userSchema, loginSchema } = require('./schemas');
+const { userRouter, loginRouter } = require('./routes/index.routes');
 
 const app = express();
 app.use(express.json());
