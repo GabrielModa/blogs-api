7d1adaf43b56d271d440d043e1934685b8858fec
Gabriel Moda <gabriel-moda@hotmail.com.br>
2022-03-19 04:04:09 -0300

    06 OK

--- controllers/categories.js
+++ controllers/categories.js
@@ -8,6 +8,13 @@ const postCategory = async (req, res) => {
   return res.status(201).json(newCategory);
 };
 
+const getCategories = async (_req, res) => {
+  const getAll = await Category.findAll();
+
+  res.status(200).json(getAll);
+ };
+
 module.exports = {
   postCategory,
+  getCategories,
 };
--- routes/categories.routes.js
+++ routes/categories.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { postCategory } = require('../controllers/categories');
+const { postCategory, getCategories } = require('../controllers/categories');
 const { schemaHandler, tokenValidate } = require('../middleware');
 const categoriesSchema = require('../schemas/categoriesSchema');
 
@@ -7,5 +7,7 @@ const router = express.Router();
 
 router.post('/', schemaHandler(categoriesSchema), 
 tokenValidate, postCategory);
+
+router.get('/', tokenValidate, getCategories);
 
 module.exports = router;
