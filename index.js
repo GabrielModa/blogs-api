da7c85ddc8dd564ac57ce809bdfe3adf72e2be44
Gabriel Moda <gabriel-moda@hotmail.com.br>
2022-03-18 21:57:15 -0300

    03 ok ?

--- index.js
+++ index.js
@@ -8,20 +8,19 @@ const { userRouter, loginRouter } = require('./routes/index.routes');
 const app = express();
 app.use(express.json());
 
-// não remova esse endpoint, e para o avaliador funcionar
-
 app.use('/user',
 userRouter);
 
 app.use('/login',
 loginRouter);
 
+app.use(errorHandler);
+
+// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
 
-app.use(errorHandler);
-
-app.listen(process.env.PORT, () => {
-  console.log(`ouvindo porta ${process.env.PORT}`);
+app.listen(3000, () => {
+  console.log('ouvindo porta 3000');
 });
